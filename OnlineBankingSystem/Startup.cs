610e9f0bd82ed166565c962bdd7c579138fb035a
yusuf <afolesmith@gmail.com>
2020-03-31 17:54:21 +0100

    Add project files.

---
+++ OnlineBankingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineBankingSystem.Startup))]
+namespace OnlineBankingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
